b62d45e820fb576b3adc3d2aa7ec983349724438
k-mozzi <kimkm0405@gmail.com>
2023-12-12 19:25:26 +0900

    update MemoryMemberRepository

---
+++ src/test/java/teamproject/usedmarket/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,27 @@
+package teamproject.usedmarket.repository;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+class MemoryMemberRepositoryTest {
+
+    @Test
+    void save() {
+
+    }
+
+    @Test
+    void findByMemberId() {
+    }
+
+    @Test
+    void findByLoginId() {
+    }
+
+    @Test
+    void findAll() {
+    }
+}
