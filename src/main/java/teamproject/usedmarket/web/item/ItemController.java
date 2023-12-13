f5e3f8726edaca179a6a5956f2b70e93db08c8db
An-sangdo <akdhrtl1@naver.com>
2023-12-13 22:03:16 +0900

    등록폼 + 아이템컨트롤러구현

--- src/main/java/teamproject/usedmarket/web/item/ItemController.java
+++ src/main/java/teamproject/usedmarket/web/item/ItemController.java
@@ -1,15 +1,22 @@
 package teamproject.usedmarket.web.item;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import teamproject.usedmarket.domain.item.Item;
+import teamproject.usedmarket.domain.item.ItemStatus;
+import teamproject.usedmarket.domain.item.ItemType;
 import teamproject.usedmarket.repository.ItemRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@Slf4j
+@RequestMapping("/items")
 @RequiredArgsConstructor
 public class ItemController {
 
@@ -17,10 +24,46 @@ public class ItemController {
     private final ItemRepository itemRepository;
 
 
-    @GetMapping("/items")
+    @GetMapping
     public String items(Model model) {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "item/items";
+    }
+
+    @GetMapping("/add")
+    public String addForm(Model model) {
+        model.addAttribute("item", new Item());
+        return "item/addForm";
+    }
+
+    @PostMapping("/add")
+    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+
+
+        log.info("item.itemType={}", item.getCategory());
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getItemId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/items/{itemId}";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findByItemId(itemId);
+        model.addAttribute("item", item);
+        return "item/item";
+    }
+
+
+    @ModelAttribute("itemTypes")
+    public ItemType[] itemTypes() {
+        return ItemType.values();
+    }
+
+    @ModelAttribute("itemStatuses")
+    public ItemStatus[] itemStatuses() {
+        return ItemStatus.values();
     }
 }
