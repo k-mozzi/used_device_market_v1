36763896dfb2e53b96a7a7f7200e041eb1c21ab3
k-mozzi <kimkm0405@gmail.com>
2023-12-15 14:51:49 +0900

    update MemberController, addMemberForm.html

--- src/main/java/teamproject/usedmarket/config/JdbcTemplateConfig.java
+++ src/main/java/teamproject/usedmarket/config/JdbcTemplateConfig.java
@@ -1,29 +1,29 @@
-package teamproject.usedmarket.config;
-
-
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import teamproject.usedmarket.repository.ItemRepository;
-import teamproject.usedmarket.repository.jdbctemplate.JdbcTemplateItemRepository;
-
-import javax.sql.DataSource;
-
-@Configuration
-@RequiredArgsConstructor
-public class JdbcTemplateConfig {
-    private final DataSource dataSource;
-
-    @Bean
-    public ItemRepository itemRepository() {
-        return new JdbcTemplateItemRepository(itemRepository());
-    }
-
-    @Bean
-    public ItemRepository itemRepository() {
-        return new JdbcTemplateItemRepository(dataSource);
-    }
-
-}
-
+//package teamproject.usedmarket.config;
+//
+//
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import teamproject.usedmarket.repository.ItemRepository;
+//import teamproject.usedmarket.repository.jdbctemplate.JdbcTemplateItemRepository;
+//
+//import javax.sql.DataSource;
+//
+//@Configuration
+//@RequiredArgsConstructor
+//public class JdbcTemplateConfig {
+//    private final DataSource dataSource;
+//
+//    @Bean
+//    public ItemRepository itemRepository() {
+//        return new JdbcTemplateItemRepository(itemRepository());
+//    }
+//
+//    @Bean
+//    public ItemRepository itemRepository() {
+//        return new JdbcTemplateItemRepository(dataSource);
+//    }
+//
+//}
+//
